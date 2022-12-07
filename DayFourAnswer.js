6915f0560d167e96f03fb50cf1d6a7be3465c266
Brendan Jarvis <jarvisbrendanj@gmail.com>
2022-12-07 15:50:39 +1300

    Day Four part one

--- DayFourAnswer.js
+++ DayFourAnswer.js
@@ -1,5 +1,21 @@
 import { readFileSync } from 'fs'
 
-const input = readFileSync('DayFourInput.txt').toString().split('\n')
-
-console.log(input)
+console.log(
+  readFileSync('DayFourInput.txt')
+    .toString()
+    .split('\n')
+    .map((pair) => {
+      const [elf1Range, elf2Range] = pair.split(',')
+      const [elf1RangeFrom, elf1RangeTo] = elf1Range
+        .split('-')
+        .map((val) => Number(val))
+      const [elf2RangeFrom, elf2RangeTo] = elf2Range
+        .split('-')
+        .map((val) => Number(val))
+      return (
+        (elf1RangeFrom >= elf2RangeFrom && elf1RangeTo <= elf2RangeTo) ||
+        (elf2RangeFrom >= elf1RangeFrom && elf2RangeTo <= elf1RangeTo)
+      )
+    })
+    .filter((contained) => contained).length
+)
