70fa525b7253b7302c99cb07ef58853919bbc8c1
Brendan Jarvis <jarvisbrendanj@gmail.com>
2022-12-13 11:28:13 +1300

    Day Four Part Two

--- DayFourAnswer.js
+++ DayFourAnswer.js
@@ -19,3 +19,25 @@ console.log(
     })
     .filter((contained) => contained).length
 )
+
+console.log(
+  readFileSync('DayFourInput.txt')
+    .toString()
+    .split('\n')
+    .map((pair) => {
+      const [elf1Range, elf2Range] = pair.split(',')
+      const [elf1RangeFrom, elf1RangeTo] = elf1Range
+        .split('-')
+        .map((val) => Number(val))
+      const [elf2RangeFrom, elf2RangeTo] = elf2Range
+        .split('-')
+        .map((val) => Number(val))
+      return (
+        (elf2RangeFrom <= elf1RangeFrom && elf1RangeFrom <= elf2RangeTo) ||
+        (elf2RangeFrom <= elf1RangeTo && elf1RangeTo <= elf2RangeTo) ||
+        (elf1RangeFrom <= elf2RangeFrom && elf2RangeFrom <= elf1RangeTo) ||
+        (elf1RangeFrom <= elf2RangeTo && elf2RangeTo <= elf1RangeTo)
+      )
+    })
+    .filter((contained) => contained).length
+)
