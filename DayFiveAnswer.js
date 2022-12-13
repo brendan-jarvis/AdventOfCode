144766cc0f5cb0f4d178f3d8674d00d5f21a48cd
Brendan Jarvis <jarvisbrendanj@gmail.com>
2022-12-13 18:11:19 +1300

    Day 5 part one answer

--- DayFiveAnswer.js
+++ DayFiveAnswer.js
@@ -12,6 +12,20 @@ const stacks = [
   'FQL',
   'WMHTDLFV',
   'LPBVMJF',
-]
+].map((stack) => stack.split(''))
 
-console.table(stacks)
+const moveN = (from, to, n) =>
+  stacks[to].push(...stacks[from].splice(-n).reverse())
+
+for (const line of input.split('\n')) {
+  const x = line.split(' ')
+  const n = +x[1]
+  const from = +x[3]
+  const to = +x[5]
+
+  console.log({ from, to, n })
+
+  moveN(from - 1, to - 1, n)
+}
+
+console.log(stacks.map((s) => s.reverse()[0]))
